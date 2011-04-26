deb32fca85c9c323fd4960c7b4c10f7e6cc2dbe7
Steve Odom <steve.odom@gmail.com>
2011-04-26 06:20:51 -0500

    adding linkedin

--- javascripts/application.js
+++ javascripts/application.js
@@ -140,9 +140,9 @@ var kravitz = {
 			var postal = li.attr("data-postal");
 			var country = li.attr("data-country").toLowerCase();
 			var li = $.jStorage.get(name[0]+"-"+name[1]);
-			console.info(name[0]+"-"+name[1]);
-			console.info(li)
 			if(!li){
+				console.info(name[0]+"-"+name[1]);
+				console.info(li);
 				IN.API.PeopleSearch()
 						.fields("id","first-name","last-name","industry","positions:(title)")
 				    .params({"first-name": name[0], "last-name": name[1], "count": 1, "country-code": country, "postal-code": postal})
