f46ed46dadf98922da1fdc4a11699d5cf3b46604
Steve Odom <steve.odom@gmail.com>
2011-04-27 06:44:50 -0500

    adding linkedin for person

---
+++ javascripts/templates/flickr.js
@@ -0,0 +1,5 @@
+var flickrResultTemplate =  '<li>' +
+'<div class="photo">' +
+'  <a href="http://www.flickr.com/people/${owner}/"><img src="http://farm${farm}.static.flickr.com/${server}/${id}_${secret}_s.jpg" /></a>' +
+'</div>' +
+'</li>';
