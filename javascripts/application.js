448dbb1eb92f937a03abf000328182358c38c4d0
Steve Odom <steve.odom@gmail.com>
2011-04-27 07:26:09 -0500

    adding linkedin for person

--- javascripts/application.js
+++ javascripts/application.js
@@ -131,7 +131,7 @@ var kravitz = {
 		},
 		target : function() {
 			tw = kravitz.twitter.model;	
-			console.info(tw.length)
+			console.info(tw.id.length)
 			if (tw.length) {
 				console.info("here")
 				var instorage = $.jStorage.get(tw.id+'');
