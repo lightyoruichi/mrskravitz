96b467896f87beada163689f643b1f3643636ee5
Steve Odom <steve.odom@gmail.com>
2011-04-27 09:31:37 -0500

    adding linkedin for person

--- javascripts/application.js
+++ javascripts/application.js
@@ -335,8 +335,7 @@ var kravitz = {
 				model.name = data.name;
 				model.location = data.location;
 				model.sn = data.screen_name;
-				console.info("from twitter: " + model.location);
-				if (IN.User.isAuthorized()) {kravitz.li.target;}
+				if (IN.User.isAuthorized()) {kravitz.li.target();}
 				
 				target = $('#background div.bio');
 				var followers = $('h5.followers span');
