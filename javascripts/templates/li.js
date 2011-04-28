b7a2ca8c3636516d9a18accf93ee7af08d2f0797
Steve Odom <steve.odom@gmail.com>
2011-04-28 10:02:26 -0500

    adding background

--- javascripts/templates/li.js
+++ javascripts/templates/li.js
@@ -1,4 +1,4 @@
 var liResultTemplate =  '<li>' +
 '${title}' +
-' {{if clips}}<em> for <a href="http://www.google.com/search?q=${company.name}">${company.name}</a></em>{{/if}}' +
+' {{if company}}<em> for <a href="http://www.google.com/search?q=${company.name}">${company.name}</a></em>{{/if}}' +
 '</li>';
