968aa54c22d173e5554cbd22e87fd9c2932af787
Steve Odom <steve.odom@gmail.com>
2011-04-30 07:38:24 -0500

    adding plancast

--- javascripts/application.js
+++ javascripts/application.js
@@ -573,6 +573,7 @@ console.info(data)
 							 data: params
 			      });
 			// hack for jquery not handling jsonp errors well.
+		console.info(req)
 			if (req.success) {
 				req.success(function(data, textStatus){
 					return callbacks.success(data, callbacks.user_data);
