6c6d3377fccecc5f585ee9bb523c1a0e7ca3b448
Steve Odom <steve.odom@gmail.com>
2011-05-03 09:55:17 -0500

    adding static map

--- javascripts/application.js
+++ javascripts/application.js
@@ -375,9 +375,12 @@ var kravitz = {
 				params = params.concat("&" + k + "=" + options[k]);
 			}
 			
+			var size = "small";
+			
 			for (var k in kravitz.li.locations) {
 				var target = $("li[data-woeid = " + k + "]");
-				params = params.concat("&markers=icon:http://kravitz.me/images/markersmall.png|shadow:false|" + target.attr('data-lat') + "," + target.attr('data-lng') );
+				// if (top == target) { }
+				params = params.concat("&markers=icon:http://kravitz.me/images/marker" + size + ".png|shadow:false|" + target.attr('data-lat') + "," + target.attr('data-lng') );
 			};
 			
 			// console.info(options);
