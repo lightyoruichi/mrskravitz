9946ac45e9463d1506463378306d756a087f1247
Steve Odom <steve.odom@gmail.com>
2011-05-04 08:06:45 -0500

    changing the length of industry chart bars

--- javascripts/application.js
+++ javascripts/application.js
@@ -299,7 +299,7 @@ var kravitz = {
 					li.children('span').css({backgroundColor: "#" + kravitz.twitter.background});
 				}
 				$('#industry_chart li').each(function(){
-					base = 30 + Math.pow(kravitz.li.industry_count, 2);
+					var base = 30 + Math.pow(kravitz.li.industry_count, 2);
 					console.info(base)
 					pct = $(this).attr('data-cnt') / kravitz.li.industry_count * 100 + base;
 					$(this).children('span').animate({width:pct + "%"}, "fast");
