76bc2074d48f35aa8759ae86b3c0db634c56afcf
Steve Odom <steve.odom@gmail.com>
2011-05-14 11:27:45 -0500

    started to experiment with twilio

--- javascripts/temp.js
+++ javascripts/temp.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-	var repeat = 100;
+	// var repeat = 100;
 	
-	$(document).everyTime(6000, function(i) {
-			var d = new Date();
-			$('#holder').append("<li>" + d.getTime() + "</li>");
-	}, repeat);
+	// $(document).everyTime(6000, function(i) {
+	// 			var d = new Date();
+	// 			$('#holder').append("<li>" + d.getTime() + "</li>");
+	// 	}, repeat);
 });
