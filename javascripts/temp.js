c7b4893f3eeeb496d432fa890222f4950b493465
Steve Odom <steve.odom@gmail.com>
2011-05-16 13:02:47 -0500

    convert to hsh

--- javascripts/temp.js
+++ javascripts/temp.js
@@ -5,9 +5,9 @@ $(document).ready(function(){
 	var anchorName = document.location.hash.substring(1);
 	if (anchorName.length > 0) {
 		cleaned = location.hash.replace("#!", "");
-		console.info(cleaned);
+		// console.info(cleaned);
 		sender_phone = base64.decode(cleaned);
-		console.info(sender_phone)
+		// console.info(sender_phone)
 		// return true;
 	}
 	
@@ -16,6 +16,14 @@ $(document).ready(function(){
 		// get subjects phone number
 		// turn that into a hsh that can be decoded and split back into the params.
 	alert("what");	
+		// for each phone number submitted...
+		// post to lolla-node server where it will send a sms with link to map with the hash to the phone numbers
+		var loc = pin.getLatLng();
+		console.info("here")
+		var str = sender_phone + "_" + loc.lat + "_" + loc.lng; 
+		var hsh = base64.encode(str)
+		console.info(str);
+		console.info(hsh);
 		return false;
 	});
 	
