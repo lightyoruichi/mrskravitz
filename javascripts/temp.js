2b933c143d6c7dae2f2c9eac3d3cce159ad32543
Steve Odom <steve.odom@gmail.com>
2011-05-16 13:40:02 -0500

    added post to mailer

--- javascripts/temp.js
+++ javascripts/temp.js
@@ -12,19 +12,39 @@ $(document).ready(function(){
 	}
 	
 	$('#form_container form').submit(function(){
-		// get lat/lng
-		// get subjects phone number
-		// turn that into a hsh that can be decoded and split back into the params.
-	alert("what");	
-		// for each phone number submitted...
-		// post to lolla-node server where it will send a sms with link to map with the hash to the phone numbers
-		var loc = pin.getLatLng();
-		console.info("here")
-		var str = sender_phone + "_" + loc.lat + "_" + loc.lng; 
-		var hsh = base64.encode(str)
-		console.info(str);
-		console.info(hsh);
+		$('#submit_btn').val("Sending...");
+		lolla.mailer(sender_phone);
 		return false;
 	});
-	
 });
+
+var lolla = {
+	mailer : function(from) {
+		var loc = pin.getLatLng();
+		params = {}
+		params.lat = loc.lat;
+		params.lng = loc.lng;
+		params.from = from;
+
+		url = "http://lolla-sinatra.cloudfoundry.com/locate";
+		$.ajax({ cache: false, 
+						 type: "POST",
+						 url: url,
+						 dataType: "jsonp",
+						 data: params
+		      }).success(function(data) {lolla.mailer_callback(data)})
+						.error(function(data) {lolla.mailer_error()});
+	},
+	mailer_callback : function(data) {
+		console.info(data)
+		if(typeof(data) == null || !data){	
+			
+		}
+		else {
+			
+		}
+	},
+	mailer_error : function() {
+		$('#submit_btn').val("Error. Try again.");	
+	}
+}
