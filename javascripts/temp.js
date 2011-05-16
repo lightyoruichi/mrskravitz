43e005f9522f551f3c3ddc6ee16418d13cceff40
Steve Odom <steve.odom@gmail.com>
2011-05-15 21:35:26 -0500

    adding the form

--- javascripts/temp.js
+++ javascripts/temp.js
@@ -6,5 +6,15 @@ $(document).ready(function(){
 	// 			$('#holder').append("<li>" + d.getTime() + "</li>");
 	// 	}, repeat);
 	
+	$('#form_container form').submit(function(){
+		// get lat/lng
+		// get subjects phone number
+		// turn that into a hsh that can be decoded and split back into the params.
+		
+		// for each phone number submitted...
+		// post to lolla-node server where it will send a sms with link to map with the hash to the phone numbers
+		
+		return false;
+	});
 	
 });
