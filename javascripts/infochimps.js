c3e449c0cc8a02ce8608938b14c4d46614f93ba4
Steve Odom <steve.odom@gmail.com>
2011-06-13 11:55:33 -0500

    adding anchor

--- javascripts/infochimps.js
+++ javascripts/infochimps.js
@@ -4,5 +4,19 @@ $(document).ready(function(){
 	map.addLayer(marker);
 	
 	marker.bindPopup("THE HOSPITAL AT WESTLAKE MEDICAL CENTER");
+	
+	var anchorName = document.location.hash.substring(1);
+	if (anchorName.length > 0) {
+		path = anchorname;
+		console.info(path);
+	}
+	
+	var base_url = "http://api.infochimps.com/geo/location/infochimps/";
+	var apikey 	 = "steveodom-NQ9ITHYiLQg48FM6JEi6qLkbm69";
+	
+	// $.getJSON("http://api.bitly.com/v3/shorten", params, function(data){
+	// 					var msg = "Hey, here's a map of where I am. " + data.data.url;
+	// 					$('#message').val(msg).show();
+	// 	    });
 
 });
