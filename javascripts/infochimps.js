65ef78afe5e876138dfe86939e6c397af89b1935
Steve Odom <steve.odom@gmail.com>
2011-06-13 11:47:58 -0500

    added an infochimps js file

---
+++ javascripts/infochimps.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	var markerLocation = new L.LatLng(30.294544, -97.824532);
+	var marker = new L.Marker(markerLocation);
+	map.addLayer(marker);
+	
+	marker.bindPopup("THE HOSPITAL AT WESTLAKE MEDICAL CENTER");
+
+}
