b34bd568e7448608bb42f746d053218e88542ae8
Steve Odom <steve.odom@gmail.com>
2011-11-01 05:46:25 -0500

    long ago changes

--- javascripts/infochimps.js
+++ javascripts/infochimps.js
@@ -1,22 +1,29 @@
 $(document).ready(function(){
-	var markerLocation = new L.LatLng(30.294544, -97.824532);
-	var marker = new L.Marker(markerLocation);
-	map.addLayer(marker);
-	
-	marker.bindPopup("THE HOSPITAL AT WESTLAKE MEDICAL CENTER");
 	
 	var anchorName = document.location.hash.substring(1);
 	if (anchorName.length > 0) {
-		cleaned = anchorName.replace("!", "");
-		console.info(cleaned);
+		api_id = anchorName.replace("!", "");
 	}
 	
 	var base_url = "http://api.infochimps.com/geo/location/infochimps/";
-	var apikey 	 = "steveodom-NQ9ITHYiLQg48FM6JEi6qLkbm69";
+	var url = base_url + api_id;
+	var params = {};
+	params.apikey = "steveodom-NQ9ITHYiLQg48FM6JEi6qLkbm69";
 	
-	// $.getJSON("http://api.bitly.com/v3/shorten", params, function(data){
-	// 					var msg = "Hey, here's a map of where I am. " + data.data.url;
-	// 					$('#message').val(msg).show();
-	// 	    });
-
+	$.getJSON(url, params, function(data){
+		// console.info(data);
+		$.each(data.features, function(i, point){
+			var markerLocation = new L.LatLng(point.geometry.coordinates[1], point.geometry.coordinates[0]);
+			var marker = new L.Marker(markerLocation);
+			map.addLayer(marker);
+			var txt = "<h2 class='map'>" + point.properties.name + "</h2>";
+			txt += "<p>"+ point.properties.address + " " + point.properties.city + ", "+ point.properties.state + " " + point.properties.postal_code +"</p>";
+			marker.bindPopup(txt);
+		});
+	});
+		
 });
+
+function inside_testicles() {
+	alert("inside");
+}
